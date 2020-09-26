b9d833267176e9778d1a94aad38bec91ef18c3aa
Flekay <71941122+Flekay@users.noreply.github.com>
2020-09-26 19:46:38 +0200

    Add files via upload

---
+++ v1/specs/config.php
@@ -0,0 +1,17 @@
+<?php
+
+    $qry[0]['apikey'] = ''; // enter your apikey
+    $qry[0]['debug'] = false; // retun errors
+    $qry[0]['sozial'] = false; // load players sozial media links
+
+    $qry['gear']['type'] = false; // load players gear type
+    $qry['gear']['product'] = false; // load players gear product name
+    $qry['gear']['url'] = false; // load players gear url
+    $qry['gear']['picurl'] = false; // load players gear picture url (230x230px)
+
+    $qry['settings']['video'] = false; // load players video settings
+    $qry['settings']['mouse'] = false; // load players mouse settings
+    $qry['settings']['keybinds'] = false; // load players keybinds settings
+    $qry['settings']['crosshair'] = false; // load players crosshair settings
+    
+?>
